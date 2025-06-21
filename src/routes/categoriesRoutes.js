9fc21483dcfea7b9206b27d94549e3155b5e5ff5
kamal Idris <QuickEmailConfirm@gmail.com>
2025-06-21 03:27:50 +0200

    dissable cat

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -4,26 +4,26 @@ import { Category } from '../models/Category.js';
 const router = express.Router();
 
 // 📥 إنشاء قسم جديد (رئيسي أو فرعي)
-router.post('/', async (req, res) => {
-  try {
-    const { nameInArabic, nameInEnglish, icon, parent } = req.body;
-    // إذا لم يكن قسمًا فرعيًا، نسمح بإضافة الأيقونة
-    const categoryData = {
-      nameInArabic,
-      nameInEnglish,
-      parent: parent || null,
-    };
+// router.post('/', async (req, res) => {
+//   try {
+//     const { nameInArabic, nameInEnglish, icon, parent } = req.body;
+//     // إذا لم يكن قسمًا فرعيًا، نسمح بإضافة الأيقونة
+//     const categoryData = {
+//       nameInArabic,
+//       nameInEnglish,
+//       parent: parent || null,
+//     };
 
-    if (!parent) {
-      categoryData.icon = icon || ''; // فقط للأقسام الرئيسية
-    }
-    const category = new Category(categoryData); // إذا لم يكن هناك parent، سيتم إنشاء قسم رئيسي
-    await category.save();
-    res.status(201).json(category);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+//     if (!parent) {
+//       categoryData.icon = icon || ''; // فقط للأقسام الرئيسية
+//     }
+//     const category = new Category(categoryData); // إذا لم يكن هناك parent، سيتم إنشاء قسم رئيسي
+//     await category.save();
+//     res.status(201).json(category);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 // 📤 جلب كل الأقسام (مع الأقسام الفرعية)
 router.get('/', async (req, res) => {
